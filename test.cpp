9e3eaee359ee6a8148a6492e45d98e48eca5cacd
seoo2001 <seoo2001@naver.com>
2023-11-01 18:09:38 +0900

    setting

--- test.cpp
+++ test.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int r, c;
+int sx, sy;
+int g[5][10];
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+queue<pair<int,int>> q;
+
 int main() {
-    int a;
-    cin >> a;
-    cout << a;
-    return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> r >> c;
+    for (int i=0; i<r; i++) {
+        for (int j=0; j<c; j++) {
+            cout << g[i][j];
+        }
+        cout << '\n';
+    }
 }
