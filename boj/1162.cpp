bc6a68bcb707374e422277f08d64f9e6c0ed0e7d
seoo2001 <seoo2001@naver.com>
2023-12-02 13:30:52 +0900

    not solve

---
+++ boj/1162.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, m, k;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m >> k;
+    for(int i=0; i<m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+    }
+}
